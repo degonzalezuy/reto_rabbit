d4533f420e5f09269cfdafea580246d596332ca6
Diego Gonzalez <diego.gonzalez@sofka.com.co>
2022-12-30 11:21:38 -0300

    Creación del proyecto

---
+++ applications/app-service/src/main/java/com/sofka/broker/MainApplication.java
@@ -0,0 +1,11 @@
+package com.sofka.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
